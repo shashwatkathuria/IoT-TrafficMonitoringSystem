8b3de6a5e3a10b37e021cf82935a5fd1e1ca6622
Shashwat Kathuria <shashwatkathuria@github.com>
2020-03-13 02:39:27 +0530

    Added picamera for real time scanning of number of cars.

--- CarsCounter/carscounter.py
+++ CarsCounter/carscounter.py
@@ -1,10 +1,31 @@
+import picamera
+camera = picamera.PiCamera()
+
 import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-im = cv2.imread('./../RoadImages/cars_1.jpeg')
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
-print('Number of cars in the image is '+ str(label.count('car')))
+
+def takePicture():
+    camera.capture('snapshot.jpg')
+    print("Just took the photo")
+    
+def detectAndCount(showOutputImage = False):
+    print("Detecting cars now")
+    im = cv2.imread('./../RoadImages/cars_2.jpg')
+    bbox, label, conf = cv.detect_common_objects(im)
+    print("Done detecting. Drawing contours now.")
+    if showOutputImage == True:
+        output_image = draw_bbox(im, bbox, label, conf)
+        plt.imshow(output_image)
+        plt.show()
+    numberOfCars = label.count('car')
+    print('Number of cars in the image is ', numberOfCars)
+    
+
+choice = 1
+
+while choice == 1:
+    takePicture()
+    detectAndCount(True)
+    choice = int(input("Enter choice 1 to continue else anything else : "))
