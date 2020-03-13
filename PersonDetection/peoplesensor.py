7830d9a4a0fa467c5774a25dd2ab5f202b7a28bb
Shashwat Kathuria <shashwatkathuria@github.com>
2020-03-13 23:35:30 +0530

    .

--- PersonDetection/peoplesensor.py
+++ PersonDetection/peoplesensor.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-im = cv2.imread('./people_1.jpeg')
+im = cv2.imread('./people_2.jpeg')
 bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
