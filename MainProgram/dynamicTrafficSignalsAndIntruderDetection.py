7894b90514c867fdc9f0ded24192198dd27280e8
shashwatkathuria <shashwatkathuria@github.com>
2020-05-17 20:10:53 +0530

    Integrated intruder detection and counting cars into traffic lights analyzing thread thread. Some corner cases(Only 13 images used for now, can be extended more, but seems enough right now) and more printing of statements introduced. Image plotting code lines among with some others commented, can be uncommented for debugging purposes.

--- MainProgram/dynamicTrafficSignalsAndIntruderDetection.py
+++ MainProgram/dynamicTrafficSignalsAndIntruderDetection.py
@@ -1,6 +1,6 @@
 # Importing libraries required
 from threading import Thread
-import time
+import time, random
 import RPi.GPIO as GPIO
 import cv2
 import matplotlib.pyplot as plt
@@ -17,6 +17,9 @@ GREENS = [17, 18, 16]
 YELLOWS = [27, 23, 20]
 REDS = [22, 24, 21]
 DANGERS = [13, 19, 26]
+
+globalI = 1
+imageCount = 0
 
 # import picamera
 # camera = picamera.PiCamera()
@@ -49,12 +52,12 @@ def main():
 
     # Starting detectingAndCountingInfiniteLoopThread as a thread
     # Always running
-    detectingAndCountingInfiniteLoopThread = Thread(target = detectIntrudersAndCountCars, args = (True,))
-    detectingAndCountingInfiniteLoopThread.start()
+    # detectingAndCountingInfiniteLoopThread = Thread(target = detectIntrudersAndCountCars, args = (True,))
+    # detectingAndCountingInfiniteLoopThread.start()
 
     # Joining threads
     trafficLightsInfiniteLoopThread.join()
-    detectingAndCountingInfiniteLoopThread.join()
+    # detectingAndCountingInfiniteLoopThread.join()
 
     # Setting all GPIO Pins LOW
     for j in range(NUMBER_OF_ROADS):
@@ -63,48 +66,82 @@ def main():
         GPIO.output(REDS[j], GPIO.LOW)
         GPIO.output(DANGERS[j], GPIO.LOW)
 
-def detectIntrudersAndCountCars(showOutputImage = False):
-    """Function to report counts of cars and detect intruders on the road."""
-
-    print("Detecting cars and people now")
-    im = cv2.imread('./../Basics/PersonDetection/people_2.jpeg')
-    bbox, label, conf = cv.detect_common_objects(im)
-    print("Done detecting.")
-    if showOutputImage == True:
-        print("Drawing contours now.")
-        output_image = draw_bbox(im, bbox, label, conf)
-        plt.imshow(output_image)
-        plt.show()
-    numberOfCars = label.count('car')
-    numberOfPersons = label.count('person')
-    numberOfDogs = label.count('dog')
-    numberOfCats = label.count('cat')
-    print(label)
-    print('Number of cars in the image is ', numberOfCars)
-    print('Number of persons in the image is ', numberOfPersons)
-
-    if numberOfPersons >= 1 or numberOfDogs >= 1 or numberOfCats >= 1:
-        for j in range(NUMBER_OF_ROADS):
-            GPIO.output(DANGERS[j], GPIO.HIGH)
-        print("PEOPLE DETECTED ON ROAD!! STOP DRIVING IMMEDIATELY!!")
-    elif numberOfPersons == 0:
-        for j in range(NUMBER_OF_ROADS):
-            GPIO.output(DANGERS[j], GPIO.LOW)
+# def detectIntrudersAndCountCars(showOutputImage = False):
+#     """Function to report counts of cars and detect intruders on the road."""
+# 
+#     print("Detecting cars and people now")
+#     im = cv2.imread('./../Basics/PersonDetection/people_2.jpeg')
+#     bbox, label, conf = cv.detect_common_objects(im)
+#     print("Done detecting.")
+#     if showOutputImage == True:
+#         print("Drawing contours now.")
+#         output_image = draw_bbox(im, bbox, label, conf)
+#         plt.imshow(output_image)
+#         plt.show()
+#     numberOfCars = label.count('car')
+#     numberOfPersons = label.count('person')
+#     numberOfDogs = label.count('dog')
+#     numberOfCats = label.count('cat')
+#     print(label)
+#     print('Number of cars in the image is ', numberOfCars)
+#     print('Number of persons in the image is ', numberOfPersons)
+# 
+#     if numberOfPersons >= 1 or numberOfDogs >= 1 or numberOfCats >= 1:
+#         for j in range(NUMBER_OF_ROADS):
+#             GPIO.output(DANGERS[j], GPIO.HIGH)
+#         print("PEOPLE DETECTED ON ROAD!! STOP DRIVING IMMEDIATELY!!")
+#     else:
+#         for j in range(NUMBER_OF_ROADS):
+#             GPIO.output(DANGERS[j], GPIO.LOW)
 
 def trafficLightInfiniteLoop():
-    global roundRobinInProcess
+    global roundRobinInProcess, globalI
+    
+    
+    nameOfFile = f'./../Basics/RoadImages/r{globalI}.png'
+    im = cv2.imread(nameOfFile)
+    bbox, label, conf = cv.detect_common_objects(im)
     while True:
         if roundRobinInProcess == False:
             roundRobinInProcess = True
-            trafficLightTimesInput = [ int(element) for element in input("Enter space separated road traffic counts : ").split(" ")]
+#             output_image = draw_bbox(im, bbox, label, conf)
+#             plt.imshow(output_image)
+#             plt.show()
+#             print("The global I is ", globalI)
+            numberOfCars = label.count('car')
+            numberOfMotorcycles = label.count('motorcycle')
+            numberOfPersons = label.count('person')
+            numberOfDogs = label.count('dog')
+            numberOfCats = label.count('cat')
+#             print(label, numberOfCars, numberOfMotorcycles)
+            if numberOfPersons >= 1 or numberOfDogs >= 1 or numberOfCats >= 1:
+                for j in range(NUMBER_OF_ROADS):
+                    GPIO.output(DANGERS[j], GPIO.HIGH)
+                print("PEOPLE DETECTED ON ROAD!! STOP DRIVING IMMEDIATELY!!")
+            else:
+                for j in range(NUMBER_OF_ROADS):
+                    GPIO.output(DANGERS[j], GPIO.LOW)
+            trafficCount = numberOfCars + numberOfMotorcycles
+            trafficLightTimesInput = [trafficCount, random.randint(0, trafficCount), random.randint(trafficCount, 50)]
             greenLightTimesList = calculateTrafficLightTime(trafficLightTimesInput)
             totalTime = sum(greenLightTimesList)
-            print(greenLightTimesList, totalTime)
+            for i in range(NUMBER_OF_ROADS):
+                print(f"Traffic count on road number {i + 1}: {trafficLightTimesInput[i]}\t Time Allocated: {greenLightTimesList[i]} sec")
+#             print(trafficLightTimesInput, greenLightTimesList, totalTime)
             showLightsThread = Thread(target = greenLightsRoundRobin, args = (greenLightTimesList, ) )
             showLightsThread.start()
+            globalI += 1
+            if globalI == 14:
+                return
+            nameOfFile = f'./../Basics/RoadImages/r{globalI}.png'
+#             nameOfFile = f'./../Basics/RoadImages/p1.jpeg'
+            im = cv2.imread(nameOfFile)
+            bbox, label, conf = cv.detect_common_objects(im)
             showLightsThread.join()
-            print("Threading Function Ends!")
+#             print("Show Traffic Lights Threading Function Joins Main Thread!")
             roundRobinInProcess = False
+            
+
 
 def calculateTrafficLightTime(roadCarsCountList):
     """Function to give appropriate green light times for the roads."""
@@ -128,8 +165,8 @@ def greenLightsRoundRobin(greenLightTimesList):
     """Function to turn on appropriate GPIO traffic signals in a
        round robin fashion in the NUMBER_OF_ROADS."""
 
-    print("Inside Round Robin Function!")
-    print(greenLightTimesList)
+#     print("Inside Round Robin Function!")
+#     print(greenLightTimesList)
     for i in range(NUMBER_OF_ROADS):
         greenLightTime = greenLightTimesList[i]
 
@@ -143,8 +180,8 @@ def greenLightsRoundRobin(greenLightTimesList):
                 GPIO.output(GREENS[j], GPIO.LOW)
                 GPIO.output(YELLOWS[j], GPIO.LOW)
                 GPIO.output(REDS[j], GPIO.HIGH)
-        print("Start : ", time.time())
-        print(greenLightTime)
+        print(f"Traffic Light {i + 1} Start : ", time.time())
+        print(f"{greenLightTime} sec duration")
         time.sleep(greenLightTime - YELLOW_LIGHT_TIME)
 
         for j in range(NUMBER_OF_ROADS):
@@ -158,7 +195,7 @@ def greenLightsRoundRobin(greenLightTimesList):
                 GPIO.output(REDS[j], GPIO.HIGH)
 
         time.sleep(YELLOW_LIGHT_TIME)
-        print("End : ", time.time())
+        print(f"Traffic Light {i + 1} End : ", time.time())
     for j in range(NUMBER_OF_ROADS):
         GPIO.output(GREENS[j], GPIO.LOW)
         GPIO.output(YELLOWS[j], GPIO.LOW)
@@ -168,13 +205,9 @@ def greenLightsRoundRobin(greenLightTimesList):
 
 def takePicture():
     """Function to take a picture using PiCamera."""
-    camera.capture('snapshot.jpg')
-    print("Just took the photo")
-
-""" takePicture()
-    detectAndCount(True)
-    choice = int(input("Enter choice 1 to continue else anything else : "))
-"""
+    nameOfFile = f'./../Basics/RoadImages/r{imageCount}.jpg'
+    camera.capture(nameOfFile)
+    imageCount += 1
 
 # Calling main function
 if __name__ == "__main__":
